1a7b8ee583713fa63266c76c3fa537b481d5ee3c
Aadya <a.charlewar@student.maastrichtuniversity.nl>
2025-05-06 19:12:48 +0200

    javadoc to blood

--- src/main/java/com/cardio_generator/generators/BloodSaturationDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/BloodSaturationDataGenerator.java
@@ -4,10 +4,22 @@ import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/** 
+ * Initialises blood saturation data generator.
+ * Calculates blood saturation values based on previous values.
+ * Implements PatientDataGenerator interface.
+ * Uses unique patient IDs.
+ * Ensures saturation values stay in a good range.
+*/
 public class BloodSaturationDataGenerator implements PatientDataGenerator {
     private static final Random random = new Random();
     private int[] lastSaturationValues;
 
+    /** 
+     * Constructor for BloodSaturationDataGenerator.
+     * @param patientCount as the number of patients to be simulated. 
+     * Initializes the lastSaturationValues array with baseline saturation values  between 95 and 100.
+    */
     public BloodSaturationDataGenerator(int patientCount) {
         lastSaturationValues = new int[patientCount + 1];
 
@@ -17,6 +29,12 @@ public class BloodSaturationDataGenerator implements PatientDataGenerator {
         }
     }
 
+    /** 
+     * Generates blood saturation data based on previous values.
+     * @param patientId as the unique identifier for the patient.
+     * @param outputStrategy as the strategy for outputting data.
+     * @throws Exception for error handling and helps navigate to error.
+    */
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {

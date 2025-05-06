3dc9e4fe71817b20c56e824833daee5f9f4ce225
Sofia Tsankova <s.tsankova@student.maastrichtuniversity.nl>
2025-05-06 19:08:09 +0200

    javadoc on AlertGenerator

--- src/main/java/com/cardio_generator/generators/AlertGenerator.java
+++ src/main/java/com/cardio_generator/generators/AlertGenerator.java
@@ -6,21 +6,38 @@ import java.util.Random;
 import com.cardio_generator.outputs.OutputStrategy;
 import java.util.logging.Logger;
 
+/**
+ * Generates an alert, indicating if an alert is being triggered or resolved.
+ * The process of determining the alert for the specified patient is randomized
+ * and is output by a given strategy.
+ * Each patient has and initial alert state that influences the chances of it changing.
+ */
 public class AlertGenerator implements PatientDataGenerator {
 
     //Added a logger for error handling
     private static final Logger logger = Logger.getLogger(AlertGenerator.class.getName());
+
+    /** Random number generator. */
     public static final Random randomGenerator = new Random();
+
     // Changed variable name to camelCase
+    /** Stores the initial alert state of each patient. */
     private boolean[] alertStates; // false = resolved, true = pressed
 
+    /**
+     * Constructor that generates initial alert states for a specific number of patients.
+     * 
+     * @param patientCount number of patients
+     */
     public AlertGenerator(int patientCount) {
         alertStates = new boolean[patientCount + 1];
     }
 
-    //Added method comment
     /**
-     * Generates or resolves an alert and handles the result according to the output strategy
+     * Generates or resolves an alert and handles the result according to the output strategy.
+     * If the given patient has an alert activated, there is a 90% chance of it being resolved.
+     * If not, there is a chance of an alert getting activated
+     * according to a poisson distribution model.
      * 
      * @param patientId ID of patient
      * @param outputStrategy strategy used to output the generated data

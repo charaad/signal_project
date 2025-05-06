5b02bec2b015a886c436095213b2d1086ec55e22
Sofia Tsankova <s.tsankova@student.maastrichtuniversity.nl>
2025-05-06 18:35:17 +0200

    javadoc for OutputStrategy

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,18 @@
 package com.cardio_generator.outputs;
 
+/**
+ * Interface, that outputs data of a patient.
+ * It is implemented by classes which specify the way a patient's data is being output.
+ */
 public interface OutputStrategy {
+
+    /**
+     * Outputs data for a specific patient
+     * 
+     * @param patientId ID of the patient
+     * @param timestamp time
+     * @param label name of type of data
+     * @param data the data
+     */
     void output(int patientId, long timestamp, String label, String data);
 }

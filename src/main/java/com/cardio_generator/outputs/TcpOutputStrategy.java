cae79dff47190d52af21d7ab386a67eb7b26b9ed
Aadya <a.charlewar@student.maastrichtuniversity.nl>
2025-05-06 18:58:42 +0200

    javadoc added to TcpOutputStrategy

--- src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
@@ -6,12 +6,23 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
 
+/** 
+ * Initialises server socket.
+ * Once client connection is estabelished, sends patient data as CSV through tCP connection.
+ * Implements OutputStrategy interface.
+ * Strategy stores patient data in a TCP socket.
+ * The data format is: <code>patientId,timestamp,label,data</code>.
+*/
 public class TcpOutputStrategy implements OutputStrategy {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
 
+    /** 
+     * Constructor for TcpOutputStrategy initializes the server socket and waits for client connection.
+     * @param port the port number on which the server will listen for incoming connections.
+    */
     public TcpOutputStrategy(int port) {
         try {
             serverSocket = new ServerSocket(port);
@@ -32,6 +43,14 @@ public class TcpOutputStrategy implements OutputStrategy {
         }
     }
 
+    /**
+     * Outputs and stores patient data in a TCP socket.
+     * @param patientId as unique identifier for patients.
+     * @param timestamp as time of data generation.
+     * @param label as type of data being generated.
+     * @param data as actual data value being generated.
+     * Does nothing if no connection.
+     */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         if (out != null) {
